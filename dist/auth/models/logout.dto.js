e5f3bfe52d33c93ba8bdf3dc43474337606b41d1
RandallYan <randall.yanh@gmail.com>
2025-03-19 11:44:49 +1030

    更新 .gitignore 并停止跟踪敏感文件和编译输出

--- dist/auth/models/logout.dto.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LogoutResponseDto = void 0;
-// 无请求体 DTO
-class LogoutResponseDto {
-}
-exports.LogoutResponseDto = LogoutResponseDto;
-//# sourceMappingURL=logout.dto.js.map
--- dist/two-factor/models/disable-2fa.dto.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Disable2faResponseDto = void 0;
-// 无请求体 DTO
-class Disable2faResponseDto {
-}
-exports.Disable2faResponseDto = Disable2faResponseDto;
-//# sourceMappingURL=disable-2fa.dto.js.map
